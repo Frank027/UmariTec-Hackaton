9490f1260d25d4972eb72bab248a5ef4ea5dd248
Ricky Didier Peralta Reyes <didier_peralta@hotmail.com>
2024-04-11 18:12:21 -0600

    Creacion de clase Semaforo

---
+++ Semaforo.js
@@ -0,0 +1,14 @@
+class Semaforo {
+    constructor(tiempoRojo, tiempoVerde) {
+      this.tiempoRojo = tiempoRojo;
+      this.tiempoVerde = tiempoVerde;
+    }
+  
+    contadorDeAutos() {
+      // Método de la clase
+    }
+
+}
+  
+  // Para crear una instancia de la clase
+  const instancia = new Semaforo(valor1, valor2);
